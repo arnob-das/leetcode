ffd502cb66fbd4f100dc5daca1cece1f90f125e3
arnob-das <contact.arnobdas@gmail.com>
2024-08-13 01:44:39 +0600

    1256.encode-number.cpp

--- 1256.encode-number.cpp
+++ 1256.encode-number.cpp
@@ -1,33 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
-    string encode(vector<string>& strs) {
+    string encode(vector<string> &strs)
+    {
         string res = "";
 
-        for(int i=0;i<strs.size();i++){
-            res += to_string(strs[i].size())+"#"+strs[i];
+        for (int i = 0; i < strs.size(); i++)
+        {
+            res += to_string(strs[i].size()) + "#" + strs[i];
         }
         return res;
     }
 
-    vector<string> decode(string s) {
+    vector<string> decode(string s)
+    {
         vector<string> res;
         int i = 0;
-        while(i<s.size()){
-            int j=i;
+        while (i < s.size())
+        {
+            int j = i;
 
-            while(s[j]!='#'){
+            while (s[j] != '#')
+            {
                 j++;
             }
 
-            int length = stoi(s.substr(i,j-i));
+            int length = stoi(s.substr(i, j - i));
 
-            res.push_back(s.substr(j+1,length));
+            res.push_back(s.substr(j + 1, length));
 
-            i=j+length+1;
+            i = j + length + 1;
         }
         return res;
     }
