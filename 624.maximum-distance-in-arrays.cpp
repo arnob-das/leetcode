f4a5971a1b91f3b1b19e36a4f9623aa2746785aa
arnob-das <contact.arnobdas@gmail.com>
2024-08-16 18:06:33 +0600

    624.maximum-distance-in-arrays

--- 624.maximum-distance-in-arrays.cpp
+++ 624.maximum-distance-in-arrays.cpp
@@ -1,20 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxDistance(vector<vector<int>>& arrays) {
+    int maxDistance(vector<vector<int>> &arrays)
+    {
         int smallest = arrays[0][0];
         int biggest = arrays[0].back();
         int max_distance = 0;
 
-        for (int i = 1; i < arrays.size(); ++i) {
+        for (int i = 1; i < arrays.size(); ++i)
+        {
             max_distance = max(max_distance, abs(arrays[i].back() - smallest));
             max_distance = max(max_distance, abs(biggest - arrays[i][0]));
             smallest = min(smallest, arrays[i][0]);
             biggest = max(biggest, arrays[i].back());
-            cout<< i<< " "<< smallest<<" ";
-            cout<< i<< " "<< biggest<<" "<<endl;
         }
 
         return max_distance;
